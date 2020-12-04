e994ef2e2bb555ad978fb90d49e72907907f82bd
Cheyroseflammer <cheyroseflammer@gmail.com>
2020-12-04 11:15:32 -0800

    first commit

---
+++ stacks.js
@@ -0,0 +1,14 @@
+// NODE CLASS //
+
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+// STACK CLASS //
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+}
