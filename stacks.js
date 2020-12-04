5bf4874b8616c39bf85efa13dfc397ce10bff689
Cheyroseflammer <cheyroseflammer@gmail.com>
2020-12-04 11:33:39 -0800

    prereq set up

--- stacks.js
+++ stacks.js
@@ -11,4 +11,17 @@ class Stack {
   constructor() {
     this.top = null;
   }
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.tops;
+    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
 }
